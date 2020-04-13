51e283d07b1902bb8d57023c84aade48b2603656
Bito Mann <bito@bitomann.com>
2020-04-13 11:38:33 -0500

    chickenMonkey edit

--- ChickenMonkey.py
+++ ChickenMonkey.py
@@ -4,18 +4,17 @@ for i in range(1, 101):
     # print(i)
 
 #   For multiples of five (5, 10, 15, etc.) print "Chicken" instead of the number.
-    if i % 5 == 0:
-        print(f"Chicken")
-
 #   For the multiples of seven (7, 14, 21, etc.) print "Monkey".
-    elif i % 7 == 0:
-        print(f"Monkey")
-    
-
 #   For is which are multiples of both five and seven print "ChickenMonkey".
-    elif i % 7 == 0 and i % 5 == 0:
-        print(f"ChickenMonkey")
-
 # To determine if a number can be evenly divided by 5 or 7, use the Python modulo operator.
+    if i % 5 == 0 and i % 7 == 0:
+        print("chickenMonkey")
+    elif i % 5 == 0:
+        print("chicken")
+    elif i % 7 == 0:
+        print("monkey")
     else:
         print(i)
+
+# if i % 5 == 0 and i % 7 == 0:
+        # print("chickenMonkey")    has to come before the others to work properly.
